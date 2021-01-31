b7bcb84308c980ee627bea03f1850ff0449a518e
1nvald0ne <sterlingmccrevey11@protonmail.com>
2021-01-31 00:38:56 -0600

    made changes to the very simple js because I don't know how to make cool js

---
+++ invalid0ne2/alert.js
@@ -0,0 +1,2 @@
+let info = "Song Credits: LittleRoot town theme from Pokemon Gen 3! Turn off autoplay on your browser if you don't want the song playing. ";
+alert(info);
--- invalid0ne2/main.js
+++
@@ -1,2 +0,0 @@
-let info = "Song Credits: LittleRoot town theme from Pokemon Gen 3! Turn off autoplay on your browser if you don't want the song playing. ";
-alert(info);
