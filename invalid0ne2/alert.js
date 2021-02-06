879b5b5f707f531bac2dd6956eb742943c37f475
1nvald0ne <sterlingmccrevey11@protonmail.com>
2021-02-06 09:42:58 -0600

    made changes and added to the forms, starting php

--- invalid0ne2/alert.js
+++ invalid0ne2/alert.js
@@ -1,2 +1,3 @@
 let credits = "Song by: Blip Stream Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By Attribution 3.0 License"
-alert(credits);
+let copyright = " All images taken from google, no copyright intended."
+alert(credits + copyright);
